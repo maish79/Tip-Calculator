a7d5cf21bd9516e0878bdc973f86120807c83870
maish79 <maishsupa@yahoo.com>
2022-10-20 13:43:41 +0000

    add javascript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("connected")
+function calculate(){
+    let bill = document.getElementById("bill").value;
+    let guest = document.getElementById("guest").value;
+    let tip = document.getElementById("tip").value;
+
+    let total = (bill + (bill *(tip/100)))/guest;
+
+    document.getElementById("total").innerHTML= total;
+
+}
+
+
