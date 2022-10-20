d88e95650a07aea7428101681c1b4609e985c412
maish79 <maishsupa@yahoo.com>
2022-10-20 17:23:10 +0000

    Add javascript calculations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 function calculate(){
-    let bill = document.getElementById("bill").value;
-    let guest = document.getElementById("guest").value;
-    let tip = document.getElementById("tip").value;
+    let bill = parseInt(document.getElementById("bill").value);
+    let guest = parseInt(document.getElementById("guest").value);
+    let tip = parseInt(document.getElementById("tip").value);
 
-    let total = (bill + (bill *(tip/100)))/guest;
+    let total =(bill + (bill*(tip/100)))/guest;
 
-    document.getElementById("total").innerHTML= total;
+    document.getElementById("total").innerHTML= total.Math.floor();
+    
+    
+    }
 
-}
 
 
